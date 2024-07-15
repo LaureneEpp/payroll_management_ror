9b7bc96c8ccab88f5be78ad86e1d441ec4e1de01
Laurène Eppellé <Laurene@mbp-de-eppelle.home>
2024-07-15 22:37:22 +0200

    create deductions  model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_191028) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_203509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,8 +42,46 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_191028) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "allowances", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "deductions", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "city"
+    t.string "country"
+    t.boolean "manager", default: false
+    t.bigint "team_id", null: false
+    t.bigint "position_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["position_id"], name: "index_employees_on_position_id"
+    t.index ["team_id"], name: "index_employees_on_team_id"
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.boolean "manager"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -59,5 +97,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_191028) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "employees", "positions"
+  add_foreign_key "employees", "teams"
   add_foreign_key "teams", "departments"
 end
