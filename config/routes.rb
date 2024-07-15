23fe98e91e44c13c8e299bed32260446e3a3207c
Laurène Eppellé <Laurene@mbp-de-eppelle.home>
2024-07-15 23:15:38 +0200

    create routes + pages controller for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
-  root 'welcome#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  root 'pages#dashboard' # shortcut for the abov
+  resources :employees
+  resources :departements, only: [:index, :show] do
+    resources :teams, only: [:show]
+  end
+  
+  resources :allowances
+  resources :payslip_allowances, only: [:destroy]
+  resources :deductions
+  resources :payslip_deductions, only: [:destroy]
+  resources :payslips do
+    member do
+      get 'dowload'
+      get 'preview'
+      post 'increase_present_days'
+      post 'decrease_present_days'
+      post 'increase_absent_days'
+      post 'decrease_absent_days'
+    end
+  end
+  get 'payslips/pdf/:id', to: 'payslips#generate_payslip', as: 'generate_payslip'
+  resources :teams, only: [:index, :new, :create]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
