194043abacd2d14a77c7e78fd1aab22b6efefca0
Laurène Eppellé <Laurene@mbp-de-eppelle.home>
2024-07-17 17:32:46 +0200

    feat(departments): add all CRUD actions

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,18 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: %i[ show ]
+
+  def index
+    @tbd_dpt = Department.where(name: 'TBD')
+    @departments = Departement.all.order('name ASC').excluding(@tbd_dpt)
+    Team.new
+  end
+
+  def show
+  end
+
+  private
+  
+  def set_department
+    @department = Department.find(params[:id])
+  end
 end
