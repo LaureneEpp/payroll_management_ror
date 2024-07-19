554fc4b0c13406d7552dfa025af73b6573f3209f
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-07-19 10:37:27 +0200

    feat(views): create all views for teams

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -16,13 +16,16 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      gray: colors.gray,
       'white': '#ffffff',
       'light-rose': '#fce7f3',
       'rose-red': '#e11d48',
       'dark-rose': '#be123c',
       'light-purple': '#e9d5ff',
-      'dark-purple': '#9333ea'
+      'dark-purple': '#9333ea', 
+      'light-gray': '#f3f4f6',
+      'medium-gray': '#d1d5db',
+      'dark-gray': '#6b7280'
+
     },
   },
   plugins: [
