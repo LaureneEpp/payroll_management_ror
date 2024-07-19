f309d930ef40d2b0528754ddcecf54cd4e659e20
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-07-19 16:15:49 +0200

    feat(views): create modal component for teams with turbo hotwire

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -27,6 +27,21 @@ module.exports = {
       'dark-gray': '#6b7280'
 
     },
+    keyframes: {
+      'fade-in-up': {
+        '0%': {
+          opacity: '0',
+          transform: 'translateY(10px)'
+        },
+        '100%': {
+          opacity: '1',
+          transform: 'translateY(0)'
+        },
+      }
+    },
+    animation :{
+      'fade-in-up': 'fade-in-up 0.3s ease-in-out'
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
