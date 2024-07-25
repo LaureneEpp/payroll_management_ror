53f7a5d7dc784abd12e1dc3a22e8973219afe0f5
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-07-25 12:55:17 +0200

    fix: fix registration controller to create new employee with new user.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    # super
+    build_resource({})
+    resource.build_employee
+    respond_with resource
+
+
+  end
 
   # POST /resource
   # def create
