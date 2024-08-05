08012990bdcf358cc7e0a1fda5959694a03e2fba
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-08-05 18:36:33 +0200

    feat(dashboard): add section to display teams

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
     def dashboard
       @employees_count = Employee.count
-      # @tbd_position = Employee.where(position: 'TBD')
-      # @tbd_team = Employee.where(team: 'TBD')
-      # @tbd_dpt = Team.joins(:department).where(departments: {name: 'TBD'})
+      @tbd_dpt = Team.joins(:department).where(departments: {name: 'TBD'})
+      @departments = Department.all.order('name ASC')
+      @teams = Team.all.order('name ASC')
       @employees = Employee.all.order('last_name ASC').excluding(@tbd_employees)
       @managers = Employee.where(manager: true).order('last_name ASC')
       @employee = Employee.new
