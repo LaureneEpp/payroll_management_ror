b88d3c0abe1e7d0a77102de6ab9f291ad6e0c6ed
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-08-06 20:06:35 +0200

    refactor svg icons with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
         content_tag(:path, '', d:"M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z")
       when 'logo'
         content_tag(:path, '', d:"M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z", stroke_linecap: "round", stroke_linejoin: "round")
+      when 'menu_closed'
+        content_tag(:path, '', d:"M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5", stroke_linecap: "round", stroke_linejoin: "round")
+      when 'menu_opened'
+        content_tag(:path, '', d:"M6 18L18 6M6 6l12 12", stroke_linecap: "round", stroke_linejoin: "round")
       when 'home'
         content_tag(:path, '', d:"M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z") +
         content_tag(:path, '', d: "M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z")
