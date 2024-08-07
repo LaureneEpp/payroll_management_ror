569fecf955220367634f308a8685ebb55da6de36
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-08-07 17:10:15 +0200

    chore: clean up unused code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  after_initialize :set_default_role, if: :new_record?
 
-
   has_one :employee, dependent: :destroy
   accepts_nested_attributes_for :employee, allow_destroy: true, update_only: true
   
+  protected 
+
   enum role: [:user, :manager, :admin]
 
-  
  def set_default_role
    self.role ||= :user
  end
