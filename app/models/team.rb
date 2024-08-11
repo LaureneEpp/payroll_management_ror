9335d0760d03e634328b3dfbb1489db640f5d294
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-08-11 14:38:54 +0200

    feat(dashboard): fix validation when new manager only one per team

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,12 +4,20 @@ class Team < ApplicationRecord
     belongs_to :leader, class_name: 'Employee', foreign_key: 'user_id', optional: true
 
     validates :name, uniqueness: true
-
+    validate :unique_manager_per_team
 
     def update_leader_roles(new_leader_id, previous_leader_id = nil)
         User.find(new_leader_id).update(role: 'manager')
         if previous_leader_id && previous_leader_id != new_leader_id
           User.find(previous_leader_id).update(role: 'standard')
+        end
+    end
+    
+    private
+
+    def unique_manager_per_team
+        if Team.where(user_id: leader) .exists? && user_id != self.user_id
+          errors.add(:user_id, 'is already a manager of another team')
         end
     end
 end
