51c5434bffb45dd2352ef94b0bb3559bb6bd16f3
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-08-22 20:14:29 +0200

    fix: manager display in dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,12 @@ class PagesController < ApplicationController
       @departments = Department.all.order('name ASC')
       @teams = Team.all.order('name ASC').excluding(@tbd_team)
       @employees = Employee.all.order('last_name ASC').excluding(@tbd_employees)
-      @managers = Employee.joins(:team).where.not(teams: {user_id: nil}).order('employees.last_name ASC')
+      manager_user_ids = Team.pluck(:user_id).uniq
+      @managers_without_team = Employee.joins(:user).where(users: { role: User.roles[:manager] }).where.not(user_id: manager_user_ids).order('employees.last_name ASC')
+      @managers =  Employee.joins(:team).where(teams: { user_id: Employee.select(:user_id) }).order('employees.last_name ASC')
       @employee = Employee.new
       @tbd_employees = Employee.joins(:position, :team).where("positions.name = 'TBD' OR teams.name = 'TBD'")
       @tbd_team = Team.where(name: 'TBD')
+      @teams_without_manager = Team.where(user_id: nil).excluding(@tbd_team)
     end
 end
